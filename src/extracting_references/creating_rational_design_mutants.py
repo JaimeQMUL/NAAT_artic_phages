36a64dbac429e6dad2f05b6161c128e09e622cba
Jaime Duarte-Niemen <jaimeduarteniemen@Jaimes-MacBook-Pro.local>
2026-03-25 15:56:07 +0000

    created all identified mutants from literature

--- src/extracting_references/creating_rational_design_mutants.py
+++ src/extracting_references/creating_rational_design_mutants.py
@@ -2,10 +2,14 @@ from src.helpers.fasta_tools import read_fasta
 
 # Recreates identified mutations that improve performance identified through wet lab research
 
+########################################################################################################################
+# Single Amino acid mutations
+########################################################################################################################
+
 uvsx=read_fasta('../../data/uvsx/P04529_sequence.fasta')
 
 # (E198N, E198R, E198K, K35G and K35G/E198R)
-mutants=['E198N', 'E198R', 'E198K', 'K35G', 'K35G/E198R']
+mutants=['E198N', 'E198R', 'E198K', 'K35G', 'K35G/E198R', 'D274A']
 
 positions = {}
 
@@ -28,6 +32,33 @@ for item in positions.items():
     new_seq=''.join(uvsx_list)
     safe_name = name.replace("/", "_")
     with open(f"../../data/uvsx/{safe_name}.fasta",'w') as f:
-        f.write(f">{safe_name} Rational Design UvsX mutant https://pubs.acs.org/doi/10.1021/acs.biochem.5c00098\n{new_seq}")
+        f.write(f">{safe_name} Rational Design UvsX mutant\n{new_seq}")
 
 
+
+########################################################################################################################
+# Larger scale mutations
+########################################################################################################################
+# Getting sequence of loop 2 donor recombinase
+# IN Future develop this to recreate published mutations involving larger scale modifications, in this case a full loop swap.
+header_to_find = ">YP_003097304"
+sequence_lines = []
+found = False
+
+with open('../data/ncbi/uvsx_sequences.fasta', 'r') as f:
+    for line in f:
+        line = line.strip()
+        if line.startswith(">"):
+            if found:
+                # We've reached the next header, stop reading
+                break
+            if line.startswith(header_to_find):
+                found = True
+        elif found:
+            # Collect sequence lines
+            sequence_lines.append(line)
+
+# Combine all sequence lines into a single string
+full_sequence = "".join(sequence_lines)
+print(f"{header_to_find}:\n{full_sequence}")
+# idea for this was replicating the loop 2 swap from YP_003097304 into UvsX
--- src/pfam/pfam.py
+++ src/pfam/pfam.py
@@ -1,2 +1,4 @@
 # Find domain IDs in reference UvsX
 # I would like this to be programatic so it can be applied to any protein, getting the keys domains found in the reference proteins.
+# Would find and store pfam ids.
+# Finds the key domains in a given protein and then scans databses to extract proteins with identified domains
