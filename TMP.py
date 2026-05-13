7428234513f7099fecd86a1bc752459c488b2e7f
Jaime Duarte-Niemen <jaimeduarteniemen@Jaimes-MacBook-Pro.local>
2026-05-13 21:55:17 +0100

    added scripts to run TMP for the top_hits

---
+++ TMP.py
@@ -0,0 +1,7 @@
+from tmp_software import FastTMP, DemocraticTMP, SummaryTMP
+import pandas as pd
+
+accessions=pd.read_csv('uvsx/data/curated_database/top_hits.csv')['accession']
+
+
+
--- src/agents/agents.py
+++
@@ -1,5 +0,0 @@
-from langchain_ollama import ChatOllama
-from langgraph.graph import StateGraph, START, END
-
-OLLAMA_MODEL = "qwen3.5"
-OLLAMA_BASE_URL = "http://localhost:11434"
